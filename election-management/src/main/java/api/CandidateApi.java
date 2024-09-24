876e2e2118ea4b896f0c692c298410d391fb5f10
Rodrigo Feitosa Rodrigues <rodrigofeitosarodrigues@gmail.com>
2024-09-24 17:51:01 -0300

    TDD - Inicio Desenvolvimento - API - Gerenciamento de Candidatos - Parte 1

---
+++ election-management/src/main/java/api/CandidateApi.java
@@ -0,0 +1,32 @@
+package api;
+
+import api.dto.in.CreateCandidate;
+import api.dto.in.UpdateCandidate;
+import domain.Candidate;
+import domain.CandidateService;
+import jakarta.enterprise.context.ApplicationScoped;
+
+import java.util.List;
+
+@ApplicationScoped
+public class CandidateApi {
+    private final CandidateService service;
+
+    public CandidateApi(CandidateService service) {
+        this.service = service;
+    }
+
+    public void create(CreateCandidate dto) {
+        service.save(dto.toDomain());
+    }
+
+    public api.dto.out.Candidate update(String id, UpdateCandidate dto) {
+        service.save(dto.toDomain(id));
+        Candidate candidate = service.findById(id);
+        return api.dto.out.Candidate.fromDomain(candidate);
+    }
+
+    public List<api.dto.out.Candidate> list() {
+        return service.findAll().stream().map(api.dto.out.Candidate::fromDomain).toList();
+    }
+}
