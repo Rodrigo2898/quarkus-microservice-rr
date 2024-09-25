0a3d1fa417d391348464656d0a153fe5f7f38a9b
Rodrigo Feitosa Rodrigues <rodrigofeitosarodrigues@gmail.com>
2024-09-25 15:02:49 -0300

    TDD - Criando Candidatos - API - Gerenciamento de Candidatos - Parte 2

---
+++ election-management/src/test/java/infrastructure/resources/CandidateResourceTest.java
@@ -0,0 +1,35 @@
+package infrastructure.resources;
+
+import api.CandidateApi;
+import api.dto.in.CreateCandidate;
+import io.quarkus.test.InjectMock;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.ws.rs.core.MediaType;
+import org.instancio.Instancio;
+import org.jboss.resteasy.reactive.RestResponse;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+@QuarkusTest
+@TestHTTPEndpoint(CandidateResource.class)
+class CandidateResourceTest {
+    @InjectMock
+    CandidateApi api;
+
+    @Test
+    void create() {
+        var in = Instancio.create(CreateCandidate.class);
+
+        given().contentType(MediaType.APPLICATION_JSON).body(in)
+                        .when().post()
+                        .then().statusCode(RestResponse.StatusCode.CREATED);
+
+        verify(api).create(in);
+        verifyNoMoreInteractions(api);
+    }
+}
