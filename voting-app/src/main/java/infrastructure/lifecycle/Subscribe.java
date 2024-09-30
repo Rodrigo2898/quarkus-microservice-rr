97fd965075d60f2a3d882a5b04d1e09775093731
Rodrigo Feitosa Rodrigues <rodrigofeitosarodrigues@gmail.com>
2024-09-30 15:08:30 -0300

    Desenvolvimento, Programação reativa - Aplicação para votação - Parte 2

--- voting-app/src/main/java/infrastructure/lifecycle/Subscribe.java
+++ voting-app/src/main/java/infrastructure/lifecycle/Subscribe.java
@@ -2,8 +2,10 @@ package infrastructure.lifecycle;
 
 import domain.Election;
 import infrastructure.repositories.RedisElectionRepository;
-import io.quarkus.redis.datasource.RedisDataSource;
+import io.quarkus.redis.datasource.ReactiveRedisDataSource;
 import io.quarkus.runtime.Startup;
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.infrastructure.Infrastructure;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.jboss.logging.Logger;
 
@@ -14,13 +16,27 @@ public class Subscribe {
 
     private static final Logger LOGGER = Logger.getLogger(Subscribe.class);
 
-    public Subscribe(RedisDataSource redisDataSource, RedisElectionRepository repository) {
+    public Subscribe(ReactiveRedisDataSource reactiveRedisDataSource,
+                     RedisElectionRepository repository) {
         LOGGER.info("Startup: Subscribe");
 
-        redisDataSource.pubsub(String.class).subscribe("election", id -> {
-            LOGGER.info("Election: " + id + " received from subscription");
-            Election election = repository.findById(id);
-            LOGGER.info("Election " + election.id() + " starting");
-        });
+        Multi<String> sub = reactiveRedisDataSource.pubsub(String.class)
+                .subscribe("elections");
+
+        // Thread Pool especifica que não vai travar o Event work
+        sub.emitOn(Infrastructure.getDefaultWorkerPool())
+                .subscribe()
+                .with(id -> {
+                    LOGGER.info("Election: " + id + " received from subscription");
+                    Election election = repository.findById(id);
+                    LOGGER.info("Election " + election.id() + " starting");
+                });
+
+
+//        redisDataSource.pubsub(String.class).subscribe("elections", id -> {
+//            LOGGER.info("Election: " + id + " received from subscription");
+//            Election election = repository.findById(id);
+//            LOGGER.info("Election " + election.id() + " starting");
+//        });
     }
 }
