4ef1a717bb1f8830f48dc4ba78bbb827b701a52e
Rodrigo Feitosa Rodrigues <rodrigofeitosarodrigues@gmail.com>
2024-10-01 15:51:05 -0300

    Desenvolvimento - Gerenciamento de Eleição - Sincronismo

--- election-management/src/main/java/infrastructure/repositories/RedisElectionRepository.java
+++ election-management/src/main/java/infrastructure/repositories/RedisElectionRepository.java
@@ -1,12 +1,16 @@
 package infrastructure.repositories;
 
+import domain.Candidate;
 import domain.Election;
 import domain.ElectionRepository;
 import io.quarkus.redis.datasource.RedisDataSource;
 import io.quarkus.redis.datasource.pubsub.PubSubCommands;
+import io.quarkus.redis.datasource.sortedset.ScoreRange;
+import io.quarkus.redis.datasource.sortedset.ScoredValue;
 import io.quarkus.redis.datasource.sortedset.SortedSetCommands;
 import jakarta.enterprise.context.ApplicationScoped;
 
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -31,5 +35,24 @@ public class RedisElectionRepository implements ElectionRepository {
 
         commands.zadd("election:" + election.id(), rank);
         pubSubCommands.publish("elections", election.id());
+    }
+
+    public Election sync(Election election) {
+        List<ScoredValue<String>> scoredValues = commands
+                .zrangebyscoreWithScores("election:" + election.id(),
+                                        ScoreRange.from(Integer.MIN_VALUE, Integer.MAX_VALUE));
+
+        var map = scoredValues.stream().map(scoredValue -> {
+            Candidate candidate = election
+                                        .votes()
+                                        .keySet()
+                                        .stream()
+                                        .filter(c -> c.id().equals(scoredValue.value()))
+                                        .findFirst()
+                                        .orElseThrow();
+            return Map.entry(candidate, (int) scoredValue.score());
+        }).toArray(Map.Entry[]::new);
+
+        return new Election(election.id(), Map.ofEntries(map));
     }
 }
---
+++ election-management/src/main/java/infrastructure/schedulers/Sync.java
@@ -0,0 +1,31 @@
+package infrastructure.schedulers;
+
+import domain.Election;
+import infrastructure.repositories.RedisElectionRepository;
+import infrastructure.repositories.SQLElectionRepository;
+import io.quarkus.scheduler.Scheduled;
+import jakarta.enterprise.context.ApplicationScoped;
+
+import java.util.List;
+
+@ApplicationScoped
+public class Sync {
+
+    private final SQLElectionRepository sqlRepository;
+    private final RedisElectionRepository redisRepository;
+
+    public Sync(SQLElectionRepository sqlRepository, RedisElectionRepository redisRepository) {
+        this.sqlRepository = sqlRepository;
+        this.redisRepository = redisRepository;
+    }
+
+    @Scheduled(cron = "*/10 * * * * ?")
+    public void sync() {
+        List<Election> elections = sqlRepository.findAll();
+
+        elections.forEach(election -> {
+            Election updated = redisRepository.sync(election);
+            sqlRepository.sync(updated);
+        });
+    }
+}
